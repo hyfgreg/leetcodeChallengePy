1546e2b1519bfd96bf6b9ace6098730137d0fce5
hyfgreg <hyfgreg@163.com>
2021-08-14 22:30:40 +0800

    update

---
+++ problems/31next-permutation.py
@@ -0,0 +1,74 @@
+"""
+31. 下一个排列
+实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。
+
+如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+
+必须 原地 修改，只允许使用额外常数空间。
+
+
+
+示例 1：
+
+输入：nums = [1,2,3]
+输出：[1,3,2]
+示例 2：
+
+输入：nums = [3,2,1]
+输出：[1,2,3]
+示例 3：
+
+输入：nums = [1,1,5]
+输出：[1,5,1]
+示例 4：
+
+输入：nums = [1]
+输出：[1]
+
+
+提示：
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+通过次数196,317提交次数526,777
+
+tag: 数组 双指针
+
+背诵, 数学证明？？？
+
+1. Find the largest index k such that a[k] < a[k + 1]. If no such index exists, the permutation is the last permutation.
+2. Find the largest index l greater than k such that a[k] < a[l].
+3. Swap the value of a[k] with that of a[l].
+4. Reverse the sequence from a[k + 1] up to and including the final element a[n].
+"""
+from typing import List
+
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0:
+            if nums[i] < nums[i + 1]:
+                break
+            i -= 1
+        if i >= 0:
+            for j in range(len(nums) - 1, i, -1):
+                if nums[j] > nums[i]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    break
+        left = i + 1
+        right = len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+
+if __name__ == '__main__':
+    nums = [1, 2]
+    s = Solution()
+    s.nextPermutation(nums)
+    print(nums)
