c8f4f88251c2b89bf598a6b29be0f499ab5eb401
hyfgreg <hyfgreg@163.com>
2021-08-30 23:21:19 +0800

    update

--- problems/34find-first-and-last-position-of-element-in-sorted-array.py
+++ problems/34find-first-and-last-position-of-element-in-sorted-array.py
@@ -38,4 +38,59 @@ from typing import List
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        pass
+        left, right = 0, len(nums) - 1
+        ret = [-1, -1]
+        while left <= right:
+            if ret[0] != -1 and ret[1] != -1:
+                break
+
+            mid = left + (right - left) // 2
+            # print(f"left {left} right {right} mid {mid}")
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                # nums[mid] == target
+                if ret[1] == -1 and (mid == len(nums) - 1 or nums[mid + 1] != target):
+                    ret[1] = mid
+                    # print(f"set ret[1] {ret[1]}")
+                    left = 0
+                    right = mid
+                    continue
+                elif ret[0] == -1 and (mid == 0 or nums[mid - 1] != target):
+                    # print(f"set ret[0] {ret[0]}")
+                    ret[0] = mid
+                    left = mid
+                    right = len(nums) - 1
+                    continue
+
+                if ret[0] == -1 and ret[1] != -1:
+                    right = mid - 1 if mid > 0 else 0
+                elif ret[0] != -1 and ret[1] == -1:
+                    left = mid + 1 if mid < len(nums) - 1 else len(nums) - 1
+                else:
+                    left = mid + 1
+        return ret
+
+
+if __name__ == '__main__':
+    s = Solution()
+    nums = [5, 7, 7, 8, 8, 10]
+    target = 8
+    print(s.searchRange(nums, target))
+    nums = [5, 7, 7, 8, 8, 10]
+    target = 6
+    print(s.searchRange(nums, target))
+    nums = []
+    target = 0
+    print(s.searchRange(nums, target))
+    nums = [1]
+    target = 1
+    print(s.searchRange(nums, target))
+    nums = [2, 2]
+    target = 2
+    print(s.searchRange(nums, target))
+    nums = [1, 4]
+    target = 4
+    print(s.searchRange(nums, target))
