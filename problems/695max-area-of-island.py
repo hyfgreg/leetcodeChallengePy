6fd3179116e3605a039f59eb376e076e6d7f1954
hyfgreg <hyfgreg@163.com>
2021-09-07 22:29:33 +0800

    update

--- problems/695max-area-of-island.py
+++ problems/695max-area-of-island.py
@@ -34,8 +34,99 @@
 tag: 深度优先搜索 广度优先搜索 并查集 数组 矩阵
 """
 from typing import List
+from collections import deque
 
 
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        pass
+        m = len(grid)
+        if m == 0:
+            return 0
+        n = len(grid[0])
+        if n == 0:
+            return 0
+        visited = []
+        for i in range(m):
+            tmp = []
+            for j in range(n):
+                tmp.append(False)
+            visited.append(tmp)
+
+        def bfs(i, j, m, n, grid: List[List[int]]):
+            nonlocal visited
+            area = 0
+            q = deque()
+            q.appendleft((i, j))
+            while q:
+                sz = len(q)
+                for i in range(sz):
+                    i, j = q.pop()
+                    if grid[i][j] == 1 and not visited[i][j]:
+                        if i > 0:
+                            q.appendleft((i - 1, j))
+                        if j > 0:
+                            q.appendleft((i, j - 1))
+                        if i < m - 1:
+                            q.appendleft((i + 1, j))
+                        if j < n - 1:
+                            q.appendleft((i, j + 1))
+                        visited[i][j] = True
+                        area += 1
+            return area
+
+        max_area = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and not visited[i][j]:
+                    max_area = max(bfs(i, j, m, n, grid), max_area)
+        return max_area
+
+    def maxAreaOfIslandDFS(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        if m == 0:
+            return 0
+        n = len(grid[0])
+        if n == 0:
+            return 0
+        visited = []
+        for i in range(m):
+            tmp = []
+            for j in range(n):
+                tmp.append(False)
+            visited.append(tmp)
+
+        def dfs(i, j, m, n, grid: List[List[int]]) -> int:
+            nonlocal visited
+            if grid[i][j] == 0 or visited[i][j]:
+                return 0
+            area = 1
+            visited[i][j] = True
+            if i > 0:
+                area += dfs(i - 1, j, m, n, grid)
+            if j > 0:
+                area += dfs(i, j - 1, m, n, grid)
+            if i < m - 1:
+                area += dfs(i + 1, j, m, n, grid)
+            if j < n - 1:
+                area += dfs(i, j + 1, m, n, grid)
+            return area
+
+        max_area = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and not visited[i][j]:
+                    max_area = max(max_area, dfs(i, j, m, n, grid))
+        return max_area
+
+
+if __name__ == '__main__':
+    s = Solution()
+    grid = [[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]
+    print(s.maxAreaOfIslandDFS(grid))
